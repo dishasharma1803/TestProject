d4e1fb7d6be0ad6ee7f4f2c07e661cb7bd9d452b
HP <HP@LAPTOP-KKDCETQV>
2023-07-09 19:19:38 +0530

    second msg

---
+++ TestProject/src/Tester.java
@@ -0,0 +1,15 @@
+
+public class Tester {
+	int n1=5,n2=3;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      Tester t= new Tester();
+      t.add();
+	}
+	void add()
+	{
+	System.out.println("sum=  "+(n1+n2));
+	}
+
+}
